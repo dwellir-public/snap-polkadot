a76b6a8b2bf1339a425ebf1ec3096271357713dc
Jonathan Udd <jonathan@dwellir.com>
2023-08-16 14:10:01 +0200

    Priming polkadot binary

--- wrappers/start-polkadot.sh
+++ wrappers/start-polkadot.sh
@@ -9,17 +9,18 @@ export SNAP_CURRENT="$(realpath "${SNAP}/..")/current"
 # Create a config if its missing.
 if [ ! -f "${SNAP_COMMON}/service-arguments" ]; then
     echo "==> Creating basic service argument file ${SNAP_COMMON}/service-arguments"
-    echo "--name=Dwellir" > "${SNAP_COMMON}/service-arguments"
+    echo "--name=Unspecified_Dwellir_node" > "${SNAP_COMMON}/service-arguments"
     chmod 0644 "${SNAP_COMMON}/service-arguments"
 fi
 
 # Read the system wide configuration file
-echo "=> Using service argument file:  ${SNAP_COMMON}/service-arguments"
+echo "=> Using service argument file: ${SNAP_COMMON}/service-arguments"
 
 SERVICE_ARGS_FILE="$SNAP_COMMON/service-arguments"
 # SERVICE_ARGS=$(tr '\n' ' ' < "$SERVICE_ARGS_FILE")
 SERVICE_ARGS=$(cat "$SERVICE_ARGS_FILE")
 
+echo "=> Service arguments: ${SERVICE_ARGS}"
 # Start the service with the specified parameters
 POLKADOT="${SNAP}/bin/polkadot"
 exec "${POLKADOT}" $SERVICE_ARGS
