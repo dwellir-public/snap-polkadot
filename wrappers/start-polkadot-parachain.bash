88d790f09d4a10f2640a414237fd865a485b8cd7
Erik Lönroth <erik.lonroth@gmail.com>
2025-08-20 16:22:34 +0200

    [WIP] initial support for parachains

---
+++ wrappers/start-polkadot-parachain.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+echo "=> Preparing the system  (${SNAP_REVISION})"
+
+SERVICE_ARGS_FILE="$SNAP_COMMON/service-arguments"
+POLKADOT_BINARY_PATH="${SNAP}/bin/polkadot-parachain"
+
+SERVICE_ARGS=$(<$SERVICE_ARGS_FILE)
+eval "SERVICE_ARGS_ARRAY=($SERVICE_ARGS)"
+
+echo "=> Service arguments: ${SERVICE_ARGS}"
+# Start the service with the specified parameters
+exec "${POLKADOT_BINARY_PATH}" "${SERVICE_ARGS_ARRAY[@]}"
